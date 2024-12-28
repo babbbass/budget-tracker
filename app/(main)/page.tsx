0885d1c2bbcce39a817ef94c7bcd7c78a32e53d2
sebastien <sebastien.savan@gmail.com>
2024-12-28 11:57:35 +0100

    redirect setting if no budget

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { auth, currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
 import { Dashboard } from "@/components/dashboard/Dashboard"
+import { findAllBudgetByUser } from "@/lib/actionsBudget"
 
 export default async function Home() {
   const { userId } = await auth()
@@ -13,6 +14,15 @@ export default async function Home() {
 
   const fullName = user.firstName + " " + user.lastName
   const email = user.emailAddresses[0].emailAddress
+
+  const budgets = await findAllBudgetByUser(email)
+  const userBudgets = budgets?.categories.filter((category) => {
+    return category.budgets.length > 0
+  })
+
+  if (userBudgets?.length === 0) {
+    redirect("/dashboard/settings")
+  }
 
   return <Dashboard userId={userId} fullName={fullName} email={email} />
 }
