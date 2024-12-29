45be4b00a4446f37b195c5e6b2d8c4a1fa22faee
sebastien <sebastien.savan@gmail.com>
2024-12-29 13:34:55 +0100

    fix build

--- components/ExportButton.tsx
+++ components/ExportButton.tsx
@@ -1,19 +1,21 @@
 "use client"
 import React from "react"
 import { Upload } from "lucide-react"
-import {
-  exportToExcel,
-  exportToPDF,
-  exportToWord,
-  CategoryType,
-} from "@/lib/utils/export"
+// import {
+//   exportToExcel,
+//   exportToPDF,
+//   exportToWord,
+//   CategoryType,
+// } from "@/lib/utils/export"
 import { Button } from "./ui/button"
+import { CategoryType } from "@/types"
 
 interface ExportButtonProps {
   budgets: CategoryType[]
 }
 
 export function ExportButton({ budgets }: ExportButtonProps) {
+  console.log(budgets)
   return (
     <div className='relative inline-block'>
       <div className='group'>
@@ -25,21 +27,21 @@ export function ExportButton({ budgets }: ExportButtonProps) {
         <div className='absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 invisible group-hover:visible'>
           <div className='py-1' role='menu'>
             <button
-              onClick={() => exportToExcel(budgets)}
+              // onClick={() => exportToExcel(budgets)}
               className='block w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'
               role='menuitem'
             >
               Exporter en Excel
             </button>
             <button
-              onClick={() => exportToPDF(budgets)}
+              // onClick={() => exportToPDF(budgets)}
               className='block w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'
               role='menuitem'
             >
               Exporter en PDF
             </button>
             <button
-              onClick={() => exportToWord(budgets)}
+              // onClick={() => exportToWord(budgets)}
               className='block w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'
               role='menuitem'
             >
