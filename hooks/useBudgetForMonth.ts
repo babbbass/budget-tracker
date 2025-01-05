2e2681424420b227a935df35fdf6dcea141ca4a4
sebastien <sebastien.savan@gmail.com>
2025-01-05 20:14:19 +0100

    delete comment

--- hooks/useBudgetForMonth.ts
+++ hooks/useBudgetForMonth.ts
@@ -1,4 +1,3 @@
-// import { useQuery } from "@tanstack/react-query";
 import { useBudgetsGenericForMonth } from "@/hooks/useBudgets"
 import { useBudgetsForMonth } from "@/hooks/useMonths"
 
@@ -8,12 +7,11 @@ export const useBudgetForMonth = (email: string, month: string) => {
   const { data: budgetsForMonth, isLoading: isMonthLoading } =
     useBudgetsForMonth(email, month)
 
-  // Combiner les données si disponibles
   const combinedData =
     budgets && budgetsForMonth ? { budgets, budgetsForMonth } : null
 
   return {
     data: combinedData,
-    isLoading: isBudgetsLoading || isMonthLoading, // Loading est vrai si un des deux est encore en cours
+    isLoading: isBudgetsLoading || isMonthLoading,
   }
 }
